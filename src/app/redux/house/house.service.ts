bbf594fea13671960bf28b0cc4f3173ca84e8931
Klauda <klaudiahuzova@gmail.com>
2018-05-04 12:35:49 +0200

    redux get houses

--- src/app/redux/house/house.service.ts
+++ src/app/redux/house/house.service.ts
@@ -1,10 +1,17 @@
 import {HouseState} from '../store/store';
 import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable()
 export class HouseService {
+
+  constructor( private http: HttpClient){}
+
+  getHouses(){
+    return this.http.get('./server/server.js');
+  }
+
   static getInitialHouseState(): HouseState {
-    // to do : fetch from api
     return { house: [] };
   }
 
